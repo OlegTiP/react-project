e2621a77670d312effcdd5da2141f924a7ad4156
OlegT <oleg133485@gmail.com>
2024-05-22 16:33:48 +0300

    class=

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
 import { Routes, createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Header from './components/Header';
@@ -7,17 +10,36 @@ import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import { Route } from 'react-router';
+import { decrement, increment } from './redux/slices/filterSlice';
 
 import './scss/app.scss';
 
 export const SearchContext = React.createContext();
 
 function App() {
+  const count = useSelector((state: RootState) => state.counter.count);
+  const dispatch = useDispatch();
+
 
   const [searchValue, setSearchValue] = React.useState('');
 
   return (
     <div className="wrapper">
+
+<button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
+
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from "react-router-dom";
@@ -10,7 +15,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+   
   </BrowserRouter>
 );
 
