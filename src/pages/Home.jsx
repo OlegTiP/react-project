ce081224fb31863f37a549309a4412c77c08454e
OlegT <oleg133485@gmail.com>
2024-06-02 14:42:36 +0300

    17# асинхронные экшены

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect, useState, useContext } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
@@ -9,16 +8,14 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import { SearchContext } from '../App';
+import { fetchPizzas } from '../redux/slices/pizzaSlice';
 
 const Home = () => {
   const dispatch = useDispatch();
   const { categoryId, sort, currentPage } = useSelector(state => state.filter);
-
+  const { items, status } = useSelector(state => state.pizza);
   const { searchValue } = React.useContext(SearchContext);
    
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
@@ -28,48 +25,48 @@ const Home = () => {
     dispatch(setCurrentPage(number));
   };
 
-  // запрос данных с сервера
-  React.useEffect(() => {
-    setIsLoading(true);
+  // запрос данных с сервера c помощью axiox
+  const getPizzas = async () => {
 
-    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sort.sortProperty.replace('-', '');
+    const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
 
-    // запрос данных с сервера c помощью axiox
-    axios
-    .get(
-      `https://661d6b6498427bbbef01c82c.mockapi.io/items?limit=4&page=${currentPage}&${category}&sortBy=${sortBy}&order=${order}${search}`
-      )
-    .then(res => {
-      setItems(res.data);
-      console.log(res);
-      setIsLoading(false);
-    })
+    dispatch(fetchPizzas({
+      sortBy,
+      order, 
+      category,
+      search,
+      currentPage,
+    }))
+  };
 
-    window.scrollTo(0, 0)
-  }, [categoryId, sort.sortProperty, searchValue, currentPage])
+  useEffect(() => {
+    getPizzas();
+    window.scrollTo(0, 0);
+  }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
-   return (
-      <div className="container">
+  return (
+    <div className="container">
       <div className="content__top">
-            <Categories value={categoryId} onChangeCategory={onChangeCategory} />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              isLoading 
-              ? skeletons 
-              : pizzas
-            }
-          </div>
-          <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+        <Categories value={categoryId} onChangeCategory={onChangeCategory} />
+        <Sort />
       </div>
-   )
-}
+      <h2 className="content__title">Все пиццы</h2>
+      {
+        status == 'error' ? (<div className="container"> 
+          <h2>Произошла ошабка загрузки</h2>
+          <p>Попробуйте сделать заказ позже</p>
+        </div>) : (<div className="content__items"> {status == 'loading' ? skeletons : pizzas} </div>)
+      }
+      
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+    </div>
+  );
+};
+
 export default Home;
---
+++ src/redux/slices/pizzaSlice.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchPizzas = createAsyncThunk(
+   'pizza/fetchPizzasStatus',
+   async (params) => {
+      const {sortBy, order, category, search, currentPage} = params;
+      const { data } = await axios.get(
+         `https://661d6b6498427bbbef01c82c.mockapi.io/items?limit=4&page=${currentPage}&${category}&sortBy=${sortBy}&order=${order}${search}`
+       );
+     return data;
+   },
+ )
+
+const initialState = {
+   items: [],
+   status: 'loading',
+};
+
+const pizzaSlice = createSlice({
+   name: 'pizza',
+   initialState,
+   extraReducers: (builder) => {
+      builder
+         .addCase(fetchPizzas.pending, (state) => {
+            state.status = "loading"
+            state.items = []
+         })
+         .addCase(fetchPizzas.fulfilled, (state, action) => {
+            state.items = action.payload;
+            state.status = "success";
+         })
+         .addCase(fetchPizzas.rejected, (state) => {
+            state.status = "error"
+            state.items = []
+         })
+   }
+});
+
+export const { setItem } = pizzaSlice.actions;
+
+export default pizzaSlice.reducer;
