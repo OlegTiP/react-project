e9cf5ed17cdfb9b05466bfb327663e02fcefa9a5
OlegT <oleg133485@gmail.com>
2024-06-02 20:39:16 +0300

    #19 Outlet

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 
 import { Routes, createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
@@ -11,26 +10,20 @@ import FullPizza from './pages/FullPizza';
 import { Route } from 'react-router';
 
 import './scss/app.scss';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        {/* <div className="container"> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<FullPizza />} />
-          <Route path="*" element={<NotFound />} />
-          {/* в пути указываем адрес в урл после главной, а в element нужную страницу */}
-          {/* чтобы отобразить страницу, не указанную в списке роутов, указать в пути * */}
-        </Routes>
-
-        {/* <Home/> */}
-        {/* </div> */}
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+        {/* в пути указываем адрес в урл после главной, а в element нужную страницу */}
+        {/* чтобы отобразить страницу, не указанную в списке роутов, указать в пути * */}
+      </Route>
+    </Routes>
   );
 }
 
