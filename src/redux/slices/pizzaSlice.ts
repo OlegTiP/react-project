b30e6cb91d7fca79134444ae4cbddc98ad024035
олег <>
2024-07-01 11:20:44 +0300

    #24 промежуточный (50я минута)

--- src/redux/slices/pizzaSlice.ts
+++ src/redux/slices/pizzaSlice.ts
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from '../store';
+import { CartItem } from './cartSlice';
 
-export const fetchPizzas = createAsyncThunk(
+export const fetchPizzas = createAsyncThunk<CartItem[], Record<string, string>>(
    'pizza/fetchPizzasStatus',
-   async (params, thunkApi) => {
+   async (params) => {
       const {sortBy, order, category, search, currentPage} = params;
-      const { data } = await axios.get(
+      const { data } = await axios.get<CartItem[]>(
          `https://661d6b6498427bbbef01c82c.mockapi.io/items?limit=4&page=${currentPage}&${category}&sortBy=${sortBy}&order=${order}${search}`
        );
      return data;
